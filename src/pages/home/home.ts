f4140e46eb674a0a7f9003412cca060f716c4ad8
plainly <amauricio.alvarez@udea.edu.co>
2018-08-14 12:21:07 -0500

    adding buttons to the view

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,11 +6,49 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-
-
-
-  constructor(public navCtrl: NavController) {
+   lista=[
+     [{nombre:'nombre0', apellido:'apellido'},
+     {nombre:'nombre1231231231123', apellido:'apellido'},
+     {nombre:'nombre2', apellido:'apellido'},
+     {nombre:'nombre3', apellido:'apellido'},
+     {nombre:'nombre0', apellido:'apellido'},
+     {nombre:'nombre1', apellido:'apellido'},
+     {nombre:'nombre2', apellido:'apellido'},
+     {nombre:'nombre3', apellido:'apellido'}],
+     [{nombre:'nombre0', apellido:'apellido'},
+     {nombre:'nombre1', apellido:'apellido'},
+     {nombre:'nombre2', apellido:'apellido'},
+     {nombre:'nombre3', apellido:'apellido'},
+     {nombre:'nombre0', apellido:'apellido'},
+     {nombre:'nombre1', apellido:'apellido'},
+     {nombre:'nombre2', apellido:'apellido'},
+     {nombre:'nombre3', apellido:'apellido'}],
+     [{nombre:'nombre0', apellido:'apellido'},
+     {nombre:'nombre1', apellido:'apellido'},
+     {nombre:'nombre2', apellido:'apellido'},
+     {nombre:'nombre3', apellido:'apellido'},
+     {nombre:'nombre0', apellido:'apellido'},
+     {nombre:'nombre1', apellido:'apellido'},
+     {nombre:'nombre2', apellido:'apellido'},
+     {nombre:'nombre3', apellido:'apellido'}],
+     [{nombre:'nombre0', apellido:'apellido'},
+     {nombre:'nombre1', apellido:'apellido'},
+     {nombre:'nombre2', apellido:'apellido'},
+     {nombre:'nombre3', apellido:'apellido'},
+     {nombre:'nombre0', apellido:'apellido'},
+     {nombre:'nombre1', apellido:'apellido'},
+     {nombre:'nombre2', apellido:'apellido'},
+     {nombre:'nombre3', apellido:'apellido'}],
+     [{nombre:'nombre0', apellido:'apellido'},
+     {nombre:'nombre1', apellido:'apellido'},
+     {nombre:'nombre2', apellido:'apellido'},
+     {nombre:'nombre3', apellido:'apellido'},
+     {nombre:'nombre0', apellido:'apellido'},
+     {nombre:'nombre1', apellido:'apellido'},
+     {nombre:'nombre2', apellido:'apellido'},
+     {nombre:'nombre3', apellido:'apellido'}],
+    ];
+    constructor(public navCtrl: NavController) {
 
   }
 
