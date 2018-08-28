e77a218918d43f0ebbb0824969b240f9d0bd0bb9
wagd96 <wagd96@gmail.com>
2018-08-28 12:46:50 -0500

    Se realizó limpieza de codigo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,12 @@ import { BrowserModule } from "@angular/platform-browser";
 import { IonicApp, IonicModule, IonicErrorHandler } from "ionic-angular";
 import { MyApp } from "./app.component";
 
-import { AboutPage } from "../pages/about/about";
-import { ContactPage } from "../pages/contact/contact";
 import { HomePage } from "../pages/home/home";
-import { TabsPage } from "../pages/tabs/tabs";
 
 import { StatusBar } from "@ionic-native/status-bar";
 import { SplashScreen } from "@ionic-native/splash-screen";
 import { ValoresPage } from "../pages/valores/valores";
-let pages = [MyApp, AboutPage, ContactPage, HomePage, TabsPage, ValoresPage];
+let pages = [MyApp, HomePage,ValoresPage];
 @NgModule({
   declarations: pages,
   imports: [BrowserModule, IonicModule.forRoot(MyApp)],
