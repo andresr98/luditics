993d3dcedd989d29c61acf2c86aee70f9aff9237
Andrés Alvarez <amauricio.alvarez@udea.edu.co>
2018-09-22 12:11:21 -0500

    cambios en estudiante.ts

--- src/models/Estudiante.ts
+++ src/models/Estudiante.ts
@@ -1,8 +1,13 @@
-export interface Estudiante{
+export interface Estudiante {
   id: number;
   nombre: string;
   apellido: string;
   posicion: [number, number];
   sexo_biologico: number;
   descripcion: string;
+  impulsividad: number;
+  frustracion: number;
+  aislamiento: number;
+  solidaridad: number;
+  armonioso: number;
 }
